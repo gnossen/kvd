108401dd57c1c3f3d4a5b347d66510ec1bcee957
Richard Belleville <rbellevi@google.com>
2020-02-25 17:02:53 -0800

    Format

--- server/server.go
+++ server/server.go
@@ -2,8 +2,6 @@
 
 package main
 
-// TODO: Format!
-
 import (
 	list "container/list"
 	"context"
@@ -25,20 +23,20 @@ var (
 	port = flag.Int("port", 50051, "The server port")
 )
 
-type keyValueStore struct {
+type kvStore struct {
 	m        map[string]string
 	mu       sync.RWMutex
 	watchers map[string]*list.List // List[*chan string]
 }
 
-func newKeyValueStore() *keyValueStore {
-	var store keyValueStore
+func newKeyValueStore() *kvStore {
+	var store kvStore
 	store.m = make(map[string]string)
 	store.watchers = make(map[string]*list.List)
 	return &store
 }
 
-func (s *keyValueStore) upsertLocked(key string, value string) {
+func (s *kvStore) upsertLocked(key string, value string) {
 	s.m[key] = value
 	if watchers, exists := s.watchers[key]; exists {
 		for elem := watchers.Front(); elem != nil; elem = elem.Next() {
@@ -49,38 +47,47 @@ func (s *keyValueStore) upsertLocked(key string, value string) {
 	}
 }
 
-func (s *keyValueStore) GetRecord(ctx context.Context, request *pb.GetRecordRequest) (*pb.Record, error) {
+func (s *kvStore) GetRecord(ctx context.Context, request *pb.GetRecordRequest) (*pb.Record, error) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 	var value string
 	var exists bool
 	if value, exists = s.m[request.Name]; !exists {
-		return &pb.Record{}, status.Errorf(codes.NotFound, fmt.Sprintf("Record at key '%s' not found.", request.Name))
+		return &pb.Record{},
+			status.Errorf(codes.NotFound,
+				fmt.Sprintf("Record at key '%s' not found.",
+					request.Name))
 	}
 	return &pb.Record{Name: request.Name, Value: value}, nil
 }
 
-func (s *keyValueStore) CreateRecord(ctx context.Context, request *pb.CreateRecordRequest) (*pb.Record, error) {
+func (s *kvStore) CreateRecord(ctx context.Context, request *pb.CreateRecordRequest) (*pb.Record, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	if _, exists := s.m[request.Record.Name]; exists {
-		return &pb.Record{}, status.Errorf(codes.InvalidArgument, fmt.Sprintf("Record at key '%s already exists.", request.Record.Name))
+		return &pb.Record{},
+			status.Errorf(codes.InvalidArgument,
+				fmt.Sprintf("Record at key '%s already exists.",
+					request.Record.Name))
 	}
 	s.upsertLocked(request.Record.Name, request.Record.Value)
 	return &pb.Record{Name: request.Record.Name, Value: request.Record.Value}, nil
 }
 
-func (s *keyValueStore) UpdateRecord(ctx context.Context, request *pb.UpdateRecordRequest) (*pb.Record, error) {
+func (s *kvStore) UpdateRecord(ctx context.Context, request *pb.UpdateRecordRequest) (*pb.Record, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	if _, exists := s.m[request.Record.Name]; !exists {
-		return &pb.Record{}, status.Errorf(codes.NotFound, fmt.Sprintf("Record at key '%s' not found.", request.Record.Name))
+		return &pb.Record{},
+			status.Errorf(codes.NotFound,
+				fmt.Sprintf("Record at key '%s' not found.",
+					request.Record.Name))
 	}
 	s.upsertLocked(request.Record.Name, request.Record.Value)
 	return &pb.Record{Name: request.Record.Name, Value: request.Record.Value}, nil
 }
 
-func (s *keyValueStore) addWatcher(key string) (*chan string, *list.Element) {
+func (s *kvStore) addWatcher(key string) (*chan string, *list.Element) {
 	fmt.Printf("Adding watcher for key '%s'\n", key)
 	c := make(chan string)
 	s.mu.Lock()
@@ -93,14 +100,15 @@ func (s *keyValueStore) addWatcher(key string) (*chan string, *list.Element) {
 	return &c, elem
 }
 
-func (s *keyValueStore) removeWatcher(key string, elem *list.Element) {
+func (s *kvStore) removeWatcher(key string, elem *list.Element) {
 	fmt.Printf("Removing watcher for key '%s'\n", key)
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	s.watchers[key].Remove(elem)
 }
 
-func (s *keyValueStore) WatchRecord(request *pb.WatchRecordRequest, stream pb.KeyValueStore_WatchRecordServer) error {
+func (s *kvStore) WatchRecord(request *pb.WatchRecordRequest,
+	stream pb.KeyValueStore_WatchRecordServer) error {
 	c, elem := s.addWatcher(request.Name)
 	defer s.removeWatcher(request.Name, elem)
 	for {
